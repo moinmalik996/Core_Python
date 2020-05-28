2fe366e66e01133278877517a458d4707f331c9a
moinm <moin.malik996@gmail.com>
2020-05-28 17:37:31 +0500

    My 2nd Commit Today

---
+++ index.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+
+def check_even(num):
+    if num % 2 == 0:
+        print("Number is Even")
+    else:
+        print("Number is Odd")
+
+
+my_num = int(input("Enter Your Number  :  "))
+print("\n")
+check_even(my_num)
+
