b5f025603fb8afdb6120c543d685a9f8442f977c
moinm <moin.malik996@gmail.com>
2020-05-31 14:45:14 +0500

    OS_Module_Started

--- functions/3.find_index_check.py
+++ functions/3.find_index_check.py
@@ -1,10 +1,16 @@
+
+import random
 from find_index_module import *
 
 list1 = ['Moin Malik', 'Hamza Malik', 'Junaid Malik', 'Saad Malik']
 
-find = 'Hamza Malik'
+find = 'Malik'
 
 index = find_index(list1,find)
 
 print(index)
 print(test_import)
+
+random = random.choice(list1)
+print(random)
+
