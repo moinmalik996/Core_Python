f4361ed095c37db0c9411d1cdf959b76a2cd4afc
Moin Malik <moin.malik996@gmail.com>
2020-10-15 11:41:55 +0500

    New Commit

--- Selenium_Automation/instagram_bot.py
+++ Selenium_Automation/instagram_bot.py
@@ -10,20 +10,29 @@ webdriver = webdriver.Chrome('C:\\Users\\moinm\\Desktop\\Github\\Core_Python_PyC
 # webdriver = webdriver.Chrome(executable_path=chromedriver_path)
 sleep(2)
 
-webdriver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+webdriver.get('https://www.instagram.com')
 sleep(3)
 
-username = webdriver.find_element_by_name('username')
-username.send_keys('your_username')
-password = webdriver.find_element_by_name('password')
-password.send_keys('your_password')
+# username = webdriver.find_element_by_name('username')
+# username.send_keys('your_username')
+# password = webdriver.find_element_by_name('password')
+# password.send_keys('your_password')
 
 # button_login = webdriver.find_element_by_css_selector('#react-root > _9eogI E3X2T > SCxLW > _4_yKc > rgFsT > gr27e > EPjEi > HmktE > Igw0E > Igw0E > sqdOP > Igw0E')
-button_login = webdriver.find_element_by_class_name('y3zKF')
-button_login.sendkeys(Keys.RETURN)
+button_login = webdriver.find_element_by_class_name('KPnG0')
+button_login.click()
+
+username = webdriver.find_element_by_id('email')
+username.send_keys('moin.malik996@gmail.com')
+password = webdriver.find_element_by_id('pass')
+password.send_keys('N@!bt@t@1dm@s1')
+
+fb_login = webdriver.find_element_by_name('login')
+fb_login.click()
+
 sleep(3)
 
-notnow = webdriver.find_element_by_css_selector('body > div:nth-child(13) > div > div > div > div.mt3GC > button.aOOlW.HoLwm')
+notnow = webdriver.find_element_by_class_name('button.aOOlW.HoLwm')
 notnow.click() #comment these last 2 lines out, if you don't get a pop up asking about notifications
 
 
