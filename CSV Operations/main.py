5941a14bd010565a3580164beb6efe18c69b37c2
moinm <moin.malik996@gmail.com>
2021-01-01 16:36:00 +0500

    Everything works fine now but you may come across an ID where posts are zero showing an error ( ZeroDivisionError: float division by zero ) and then the session stops.

--- CSV Operations/main.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open("C:\\Users\\moinm\\PycharmProjects\\Core_Python\\CSV Operations\\data.csv", 'r') as readObj:
-    csv_reader = csv.DictReader(readObj)
-    for line in csv_reader:
-        print(line["Username"])
--- selenium_automations/insta_auto.py
+++ selenium_automations/insta_auto.py
@@ -19,13 +19,14 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as Wait
 from selenium.webdriver.support import expected_conditions as EC
+from instaloader import Instaloader, Profile
+
 
 # For CSV File Operations
 import csv
 from os import path
 
 
-#
 def extract_data(file_func, field_names_func, data):
     file_exist = path.exists(r'C:\Users\moinm\PycharmProjects\Core_Python\selenium_automations\Boxing.csv')
     if file_exist:
@@ -41,15 +42,19 @@ def extract_data(file_func, field_names_func, data):
             myfile.close()
 
 
+# Login Credentials
+login_id = 'influencerhunter996'
+login_pass = '!nfluencerhunter'
+
+# Enter something you want to search in IG Search Bar
+search_term = input("Enter a Niche you want to Search   :   ")
+
 headless1 = webdriver.FirefoxOptions()
 headless1.headless = True
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=headless1)
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 driver.maximize_window()
 driver.get('https://instagram.com')
-print(driver.title)
-print('Headless Browser Started')
-
-locator_wait = 5
+print(driver.title, " Session Started")
 
 # Elements Locators
 username_locator = "username"
@@ -58,18 +63,16 @@ login_locator = "#loginForm > div > div:nth-child(3) > button"
 notnow_locator = "#react-root > section > main > div > div > div > div > button"
 notnow_locator_2 = "body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.HoLwm"
 search_locator = "input[placeholder='Search']"
-ag_locator = "div.drKGC > div > a"
+ag_locator = "//span[@class='Ap253']"
 
-name_locator = "//h2"
-posts_locator = "//*[@id='react-root']/section/main/div/header/section/ul/li[1]/span/span"
-followers_locator = "//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a/span"
-following_locator = "//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span"
+locator_wait = 5
 
+# Filling Form and Log In
 username = Wait(driver, locator_wait).until(EC.presence_of_element_located((By.NAME, username_locator)))
-username.send_keys("")
+username.send_keys(login_id)
 
 password = Wait(driver, locator_wait).until(EC.presence_of_element_located((By.NAME, password_locator)))
-password.send_keys("")
+password.send_keys(login_pass)
 
 login = driver.find_element(By.CSS_SELECTOR, login_locator)
 login.click()
@@ -80,53 +83,50 @@ not_now.click()
 not_now_2 = Wait(driver, locator_wait).until(EC.presence_of_element_located((By.CSS_SELECTOR, notnow_locator_2)))
 not_now_2.click()
 
+# Enter Query in Search Bar
 search = Wait(driver, locator_wait).until(EC.presence_of_element_located((By.CSS_SELECTOR, search_locator)))
-search.send_keys("boxing")
+search.send_keys(search_term)
 
-auto_suggestions = Wait(driver, locator_wait).until(EC.visibility_of_any_elements_located((By.CSS_SELECTOR, ag_locator)))
+# Autosuggestions
+auto_suggestions = Wait(driver, locator_wait).until(EC.visibility_of_any_elements_located((By.XPATH, ag_locator)))
 
 print("\nTotal Suggestions are  :  ", len(auto_suggestions), "\n")
 
+loader = Instaloader()
+loader.login(login_id, login_pass)
+
 # Extracting IDs and Hashtags Separately
 for sugg in auto_suggestions:
-    term = "explore"
-    link = sugg.get_attribute('href')
+    suggestion = sugg.text
+    term = "#"
+    if term not in suggestion:
+        print(suggestion)
+        target_profile = suggestion
 
-    if term not in link:
-        driver.execute_script("window.open('')")
-        driver.switch_to.window(driver.window_handles[1])
-        driver.get(link)
+        profile = Profile.from_username(loader.context, target_profile)
 
-        name = Wait(driver, 10).until(EC.presence_of_element_located((By.XPATH, name_locator)))
-        posts = Wait(driver, 10).until(EC.presence_of_element_located((By.XPATH, posts_locator)))
-        followers = Wait(driver, 10).until(EC.presence_of_element_located((By.XPATH, followers_locator)))
-        following = Wait(driver, 10).until(EC.presence_of_element_located((By.XPATH, following_locator)))
+        followers = profile.followers
+        following = profile.followees
+        total_num_likes = 0
+        total_num_comments = 0
+        total_num_posts = 0
+
+        for post in profile.get_posts():
+            total_num_likes += post.likes
+            total_num_comments += post.comments
+            total_num_posts += 1
+
+        engagement_rate = float(total_num_likes + total_num_comments) / (followers * total_num_posts)
 
         file = r"C:\Users\moinm\PycharmProjects\Core_Python\selenium_automations\Boxing.csv"
-        field_names = ['NAME', 'POSTS', 'FOLLOWERS', 'FOLLOWING']
+        field_names = ['NAME', 'POSTS', 'FOLLOWERS', 'FOLLOWING', 'Engagement_Rate']
         my_dict = {
-            'NAME': name.text,
-            'POSTS': posts.text,
-            'FOLLOWERS': followers.text,
-            'FOLLOWING': following.text
+            'NAME': suggestion,
+            'POSTS': total_num_posts,
+            'FOLLOWERS': followers,
+            'FOLLOWING': following,
+            'Engagement_Rate': engagement_rate
         }
-
         extract_data(file, field_names, my_dict)
-
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-
-#     item = sugg.get_attribute('href')
-#     term = "explore"
-#     if term in item:
-#         explore.append(item)
-#     else:
-#         IDs.append(item)
-#
-# # Printing Explore URLS and Id URLS
-# print("\nExplore URLs are  :  ")
-# for exp in explore:
-#     print(exp)
-# print("\n\nID URLs are       :  ")
-# for ids in IDs:
-#     print(ids)
+    else:
+        print("Its a Hashtag")
