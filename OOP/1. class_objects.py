f27fb4c5749e90f2549fd288a6fbf1edcbb890f6
syaqoob <syaqoob@insite.net>
2022-03-23 12:50:21 -0400

    OOP Revising

---
+++ OOP/1. class_objects.py
@@ -0,0 +1,13 @@
+class Mobile():
+
+    def __init__(self):
+        self.model = "Samsung"
+
+    def show_data(self, p):
+        price = p
+        print("Model : ", self.model)
+        print("Price : ", price)
+
+
+sam = Mobile()
+sam.show_data(800)
